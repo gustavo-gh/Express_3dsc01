5292b496d999a7f15ad3157e2693ea74ee42ba61
roma <129233444+gustavo-gh@users.noreply.github.com>
2024-10-04 15:00:02 +0000

    rotas home sobre

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const app = express();
 const PORT = 3301;
 
 app.get("/", function (req, res) {
-  // res.sendFile()
-  res.send(__dirname);
+  res.sendFile(__dirname + "/src/views/index.html");
+});
+
+app.get("/sobre", function (req, res) {
+  res.sendFile(__dirname + "/src/views/about.html");
+});
+// ROTA UTILIZANDO MIDDLEWARE(404)
+app.use(function (req, res) {
+  res.status(404).sendFile(__dirname + "/src/views/404.html");
 });
 
 app.listen(PORT, () => {
